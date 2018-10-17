e0c4b7846515882c32721b80dcda1a9aedc45960
Samuel Schmid <samuel.schmid@modeso.ch>
2018-10-17 08:53:42 +0200

    load the settings registration correctly

--- includes/class-proffix-taxonomy.php
+++ includes/class-proffix-taxonomy.php
@@ -143,6 +143,7 @@ class Proffix_Taxonomy {
 		$this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_styles' );
 		$this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_scripts' );
 		$this->loader->add_action( 'admin_menu', $plugin_admin, 'add_admin_menu');
+		$this->loader->add_action( 'admin_init', $plugin_admin, 'register_settings');
 	}
 
 	/**
